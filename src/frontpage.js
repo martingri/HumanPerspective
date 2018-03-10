1b16ee3e53c0a874aa98377afa804513167b9ab3
Martingrim <martin.grimeland@gmail.com>
2018-03-10 15:22:57 +1100

    Using the app bar at the top of the page

--- src/frontpage.js
+++ src/frontpage.js
@@ -4,7 +4,6 @@ import { MuiThemeProvider, createMuiTheme, withStyles } from 'material-ui/styles
 import Paper from 'material-ui/Paper';
 import Grid from 'material-ui/Grid';
 import Hidden from 'material-ui/Hidden';
-import Logo from './components/logo';
 import BurgerMenu from './components/burger-menu';
 import HorizontalMenu from './components/horizontal-menu';
 import Blurb from './components/blurb';
@@ -12,6 +11,7 @@ import Banner from './components/banner';
 import AboutUs from './components/about-us';
 import Services from './components/services';
 import ContactUs from './components/contact-us';
+import AppBar from 'material-ui/AppBar';
 
 const topBannerImg = '835561.jpg';
 const middleBannerImg = '703700.jpg';
@@ -70,6 +70,9 @@ const theme = createMuiTheme({
       textTransform: 'uppercase',
       color: '#121212'
     },
+    display3: {
+      textTransform: 'uppercase'
+    },
     button: {
       fontFamily: 'oswald'
     },
@@ -104,21 +107,16 @@ function Frontpage(props) {
   return (
     <MuiThemeProvider theme={theme}>
       <div className={classes.root}>
-        <Grid container spacing={24}>
-          <Grid item xs={10} sm={2}>
-            <Logo />
-          </Grid>
-          <Hidden smUp>
-            <Grid item xs={2}>
-              <BurgerMenu />
-            </Grid>
-          </Hidden>
+        <Grid container>
           <Hidden xsDown>
-            <Grid item sm={8}>
+            <AppBar position="sticky" color="default">
               <HorizontalMenu />
-            </Grid>
-            <Grid item sm={2}>
-            </Grid>
+            </AppBar>
+          </Hidden>
+          <Hidden smUp>
+          <AppBar position="sticky" color="default">
+              <BurgerMenu />
+            </AppBar>
           </Hidden>
           <Grid item xs={12}>
             <Banner bannerImg={topBannerImg} bannerText="TECHNICAL AND PRODUCT SERVICES" />
