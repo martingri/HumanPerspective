dbe9770dce45171ae015e2993c9960a33c8d8dae
Martingrim <martin.grimeland@gmail.com>
2018-03-10 15:22:27 +1100

    Changing the profile component to make the image float left to the text and use the entire width of the screen

--- src/components/member-profile.js
+++ src/components/member-profile.js
@@ -11,6 +11,13 @@ const styles = theme => ({
   root: {
     flexGrow: 1,
   },
+  profileImage: {
+    float:'left',
+    paddingRight: '30px',
+    paddingBottom: '10px',
+    paddingTop: '20px',
+    width: '300px'
+  }
 });
 
 function generateEducation(education, id) {
@@ -22,32 +29,33 @@ function generateBullet(point, id) {
   return (<div key={id}><Typography variant="body1" gutterBottom align="left" key={id}><Bullet style={{ height: '10px' }} />{point}</Typography></div>);
 }
 
+function generatePitchParagraph(text, id) {
+  return (<Typography key={id} variant="body1" gutterBottom align="left">
+           {text}
+          </Typography>);
+}
+
 function MemberProfile(props) {
   const { classes } = props;
   const profile = props.profile;
   const education = profile.education;
   const expertise = profile.expertise;
   const experience = profile.experience;
+  const pitch = profile.pitch;
   const imagePath = require(`../assets/img/${profile.profileImage}`);
   return (
     <Grid container>
-      <Grid item xs={4}>
-        <img src={imagePath} className={classes.profileImage} />
-      </Grid>
-      <Grid item xs={8}>
+      <Grid item xs={12}>
+        <img src={imagePath} className={classes.profileImage}/>
         <Typography variant="display2" gutterBottom align='left'>
           {profile.name}
         </Typography>
         <Typography variant="subheading" gutterBottom align='left'>
           {profile.title}
         </Typography>
-        <Typography variant="body1" gutterBottom align="left">
-          {profile.pitch}
-        </Typography>
+        {pitch.map((text, i) => generatePitchParagraph(text, i))}
       </Grid>
-      <Grid item xs={4}>
-      </Grid>
-      <Grid item xs={8}>
+      <Grid item xs={12}>
         <Grid container>
           <Grid item xs={12} sm={6}>
             <Typography variant="display1" gutterBottom align="left">
@@ -63,7 +71,7 @@ function MemberProfile(props) {
           </Grid>
           <Grid item xs={12} sm={6}>
             <Typography variant="display1" gutterBottom align="left">
-              EXPERIENCE/STRENGTHS
+              EXPERIENCE & STRENGTHS
         </Typography>
             {expertise.map((text, i) => generateBullet(text, i))}
           </Grid>
