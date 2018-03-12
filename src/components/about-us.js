1fdc097f18df68288bf5022b43e21e3929b0984e
Martingrim <martin.grimeland@gmail.com>
2018-03-12 15:06:20 +1100

    Changing the about us text

--- src/components/about-us.js
+++ src/components/about-us.js
@@ -17,26 +17,26 @@ const styles = theme => ({
 
 function getMemberProfileComponent(profile, id) {
   return (<Grid item xs={12} key={id}>
-    <MemberProfile profile={profile} key={id}/>
+    <MemberProfile profile={profile} key={id} />
   </Grid>);
 }
 
 function AboutUs(props) {
   const { classes } = props;
   return (
-    <Grid  container id="about-us">
-        <Grid item xs={12}>
-            <Typography variant="display3" gutterBottom align='center' style={{marginTop: '40px'}}>
-              About us
-            </Typography>
-            <Typography variant="body1" gutterBottom align="left">
-            Human Perspective works on web and native applications that are made for humans and have a positive social impact. We develop our own concepts and work with like minded businesses on their products. Our expertise is product design and concept development with extensive experience shaping, designing and building ground-breaking new products. Human Perspective is based in Newcastle, Australia with strong connections to Oslo, Norway and London, UK.            </Typography>
-            <Typography variant="body1" gutterBottom align="left">
-We believe in lean product development principles and that great communication is essential for a successful project. When we’re involved in projects that requires a bigger team or specialists, we scale up the team with professionals in our network who we know and trust.
-            </Typography>
-        </Grid>
-        {profiles.map((profile, i) => getMemberProfileComponent(profile, i))}
+    <Grid container id="about-us">
+      <Grid item xs={12}>
+        <Typography variant="display3" gutterBottom align='center' style={{ marginTop: '40px' }}>
+          About us
+        </Typography>
+        <Typography variant="body1" gutterBottom align="left">
+          Human Perspective works on web and native applications that are made for humans and have a positive social impact. We develop our own concepts and work with like minded businesses on their products. With extensive experience shaping, designing and building ground-breaking new products our expertise is product design and concept development. Human Perspective is based in Newcastle, Australia with strong connections to Oslo, Norway and London, UK.</Typography>
+        <Typography variant="body1" gutterBottom align="left">
+          We believe in lean product development principles and that great communication is essential for a successful project. When we’re involved in projects that requires a bigger team or specialists, we scale up the team with professionals in our network who we know and trust.
+        </Typography>
       </Grid>
+      {profiles.map((profile, i) => getMemberProfileComponent(profile, i))}
+    </Grid>
   );
 }
 
